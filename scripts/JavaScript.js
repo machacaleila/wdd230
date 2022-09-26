22956e0ee4cb243a433ef58c9acd16b669deedc3
machacaleila <lmachaca@byu.edu>
2022-09-25 23:55:14 -0300

    chan

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -1,2 +1,21 @@
-var date = Document.lastModified;
-      Document.getElementById("modify").innerHTML = "The document was last modified on : " + date;
+
+function date(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth()+1;
+    var year = date.getFullYear();
+    var d = day + "/" + month + "/" + year;
+    document.querySelector("date").innerHTML = d;
+}
+date();
+
+function time(){
+    var date= new date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    
+    var time= hour + ":" + min + ":" + sec;
+    document.querySelector("time").innerHTML = time;
+}
+time(); 
